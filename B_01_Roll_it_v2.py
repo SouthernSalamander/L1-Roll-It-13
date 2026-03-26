bfe35e380bb1bf2c4eedb4cb552e76199feb0e8b
SouthernSalamander <trinderj4203@masseyhigh.school.nz>
2026-03-26 13:32:30 +1300

    Complete Roll It 13 Game

---
+++ B_01_Roll_it_v2.py
@@ -0,0 +1,233 @@
+import random
+import time
+
+def yes_no(question):
+
+    """Checks user response to a question is yes / no (y/n), returns 'yes' or 'no' """
+
+    while True:
+
+        response = input(question).lower()
+
+        # check if user says yes / no
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("Say yes / no")
+
+def instructions():
+
+    """Prints instructions"""
+    print("""
+*** Instructions ***
+
+Roll the dice and try to win!
+    """)
+
+def initial_points(which_player):
+    """Roll dice twice and return total / if double points apply"""
+
+    double = "no"
+
+    # Roll the dice for the user and note if they got a double
+    roll_one = random.randint(1, 6)
+    roll_two = random.randint(1, 6)
+
+    if roll_one == roll_two:
+        double = "yes"
+
+    total = roll_one + roll_two
+    print(f"{which_player} - Roll 1: {roll_one} \t| Roll 2: {roll_two} \t| Total: {total}")
+
+    return total, double
+
+def make_statement(statement, decoration):
+    """Adds emoji / additional characters to the start and end of headings"""
+
+    ends = decoration * 3
+    print(f"\n{ends} {statement} {ends}")
+
+def int_check():
+
+    """Asks user for game goal and makes sure it's above or equal to 13"""
+    error = "Please enter an integer more than / equal to 13."
+
+    while True:
+        try:
+            response = int(input("Game Goal: "))
+
+            if response < 13:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+
+# Main starts here
+
+# ask user if they want instructions (check they say yes / no)
+want_instructions = yes_no("Do you want to see the instructions? ")
+
+if want_instructions == "yes":
+    instructions()
+
+# ask user for game goal and saves it
+print()
+game_goal = int_check()
+
+# set user and cpu scores to 0 at the start of the game
+user_score = 0
+cpu_score = 0
+rounds_played = 0
+round_feedback = ""
+winner = ""
+
+game_history = []
+
+# Welcome message
+make_statement("Lets go gambling!", "🎲")
+print()
+
+# Play multiple rounds until a winner has been found
+while user_score < game_goal and cpu_score < game_goal:
+
+    # start of round
+    rounds_played += 1
+    make_statement(f"Round {rounds_played}", "=")
+
+    # Roll the dice for the user and note if they got a double
+    initial_user = initial_points("User")
+    initial_cpu = initial_points("CPU")
+
+    # Retrieve user points (first item returned from function)
+    user_points = initial_user[0]
+    cpu_points = initial_cpu[0]
+
+    double_user = initial_user[1]
+
+    # add initial rolls to points
+
+    # Let the user know if the qualify for double points
+    if double_user == "yes":
+        print("\nYou got a double!")
+        print("If you win this round, you'll get double the points")
+
+    # assume user goes first
+    first = "User"
+    second = "CPU"
+    player_1_points = user_points
+    player_2_points = cpu_points
+
+    # if user has fewer points, the start the game
+    if user_points < cpu_points:
+        print("You start because your initial roll was less than the CPU\n")
+
+    # if the user and computer roll equal points, the user is player 1...
+    elif user_points == cpu_points:
+        print("The initial rolls were the same, the user starts!")
+
+    # if the computer has fewer points, switch the computer to 'player 1'
+    else:
+        player_1_points, player_2_points = player_2_points, player_1_points
+        first, second = second, first
+
+    while player_1_points < 13 and player_2_points < 13:
+
+        print()
+        input("Press <enter> to continue this round\n")
+
+        # first person rolls the die and score is updated
+        player_1_roll = random.randint(1, 6)
+        player_1_points += player_1_roll
+
+        print(f"{first}: Rolled a {player_1_roll} - has {player_1_points} points")
+
+        # if the first person's score is over 13, end the round
+        if player_1_points >= 13:
+            break
+
+        # second person rolls the die (and score is updated)
+        player_2_roll = random.randint(1, 6)
+        player_2_points += player_2_roll
+
+        print(f"{second}: Rolled a {player_2_roll} - has {player_2_points} points")
+
+        print(f"{first}: {player_1_points} | {second}: {player_2_points}")
+
+    # end of round
+
+    # associate player points with either the user or the computer
+    user_points = player_1_points
+    cpu_points = player_2_points
+
+    # switch the user and computer points if the computer went first
+    if first == "CPU":
+        user_points, cpu_points = cpu_points, user_points
+
+    # work out who won
+    if user_points > cpu_points:
+        winner = "User"
+        cpu_points = 0
+    else:
+        winner = "CPU"
+        user_points = 0
+
+    round_feedback = f"The {winner} won!"
+
+    # work out who won and set the loser points to 0
+    if user_points > cpu_points:
+        winner = "User"
+        loser = "CPU"
+        cpu_points = 0
+    else:
+        winner = "CPU"
+        loser = "User"
+        user_points = 0
+
+    # double user points if eligible
+    if winner == "User" and double_user == "yes":
+        user_points = user_points * 2
+
+    # outputs results of round
+    make_statement("Round Results", "-")
+    print(f"User Points: {user_points} | CPU Points: {cpu_points}")
+
+    # declare the winner
+    print(round_feedback)
+    print()
+
+    # adds round's points to overall scores
+    user_score += user_points
+    cpu_score += cpu_points
+
+    # Generate round results and add it to the game history list
+    game_results = (f"Round {rounds_played}: User Points: {user_points} | "
+                    f"CPU Points: {cpu_points}, {winner} wins "
+                    f"({user_score} | {cpu_score})")
+
+    game_history.append(game_results)
+
+    # show overall scores
+    make_statement("Game Update", "*")
+    print(f"User Score: {user_score} | CPU Score: {cpu_score}")
+
+
+# end of entire game, output final results
+
+make_statement("Game Over", "🏁")
+time.sleep(1)
+
+print()
+if user_score > cpu_score:
+    make_statement("You Won!", "✅")
+else: make_statement("The CPU won", "❌")
+
+print()
+make_statement("Game History", "*")
+
+for item in game_history:
+    print(item)
